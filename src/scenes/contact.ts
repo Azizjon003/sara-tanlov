d24e2b27e3cfa0195bb453e16c4f117326c99e94
Azizjon <aliqulovazizjon68@gmail.com>
2024-08-06 11:46:28 +0500

    feat: Add phone logging for contact scene

--- src/scenes/contact.ts
+++ src/scenes/contact.ts
@@ -26,6 +26,7 @@ scene.hears("/start", async (ctx: any) => {
 
 scene.on("message", async (ctx: any) => {
   const phone = ctx.message?.contact?.phone_number || ctx.message?.text; // message text
+  console.log(phone);
   const isTest = checkUzbekPhoneNumber(phone);
 
   console.log(isTest, phone);
