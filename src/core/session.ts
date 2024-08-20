d913e20eed35556e37139daa0532fb0366587a23
azizjon003 <aliqulovazizjon68@gmail.com>
2024-08-20 15:13:33 +0500

    session for postgresql

--- src/core/session.ts
+++ src/core/session.ts
@@ -1,4 +1,11 @@
-import { session as memorySession } from "telegraf";
-const session = memorySession({});
+const PostgresSession = require("telegraf-postgres-session");
+// const session = memorySession({});
+
+const session = new PostgresSession({
+  connectionString: process.env.SESSION_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default session;
